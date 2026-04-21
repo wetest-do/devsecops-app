d1760ab55508225f0f9f68aa516ad0b08fdad498
user1 <277520732+wetest-do@users.noreply.github.com>
2026-04-21 12:33:51 +0200

    added helmet for csrf prevention

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const helmet = require('helmet');
 const app = express();
+
+app.use(helmet());
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.json({ message: 'DevSecOps Lab', status: 'ok' });
